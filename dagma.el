5901a66c47b6cca2ccc48d5dec281c7395389937
ROCKTAKEY <rocktakey@gmail.com>
2022-06-05 18:18:32 +0900

    Add primitive functions to control graph

--- dagma.el
+++ dagma.el
@@ -110,5 +110,75 @@ It is used when you make instance of `dagma-node' through `dagma--node-create'."
   (let ((children (dagma-node-children node)))
     (setf (dagma-node-children node) (cl-remove key children))))
 
+
+;;; Primitive functions to control graph
+
+(defun dagma--graph-create ()
+  "Create `dagma-graph' object."
+  (make-hash-table))
+
+(defun dagma--graph-node-get (graph key)
+  "Get `dagma-node' node from GRAPH at key KEY."
+  (cl-check-type graph hash-table)
+  (gethash key graph))
+
+(defun dagma--graph-node-get-ensure (graph key)
+  "Get `dagma-node' node from GRAPH at key KEY."
+  (cl-check-type graph hash-table)
+  (or (dagma--graph-node-get graph key)
+      (let ((node (dagma--node-create)))
+        (dagma--graph-node-put graph key node))))
+
+(defun dagma--graph-node-put (graph key node)
+  "Store `dagma-node' NODE into the node of GRAPH at key KEY."
+  (cl-check-type graph hash-table)
+  (cl-check-type node dagma-node)
+  (puthash key node graph))
+
+(defun dagma--graph-children-get (graph key)
+  "Get children on `dagma-node' node in GRAPH at key KEY."
+  (dagma-node-children (dagma--graph-node-get-ensure graph key)))
+
+(defun dagma--graph-children-put (graph key val)
+  "Store VAL as children on `dagma-node' node in GRAPH at key KEY."
+  (let ((node (dagma--graph-node-get-ensure graph key)))
+    (setf (dagma-node-children node) val)))
+
+(defun dagma--graph-parents-get (graph key)
+  "Get parents on `dagma-node' node in GRAPH at key KEY."
+  (dagma-node-parents (dagma--graph-node-get-ensure graph key)))
+
+(defun dagma--graph-parents-put (graph key val)
+  "Store VAL as parents on `dagma-node' node in GRAPH at key KEY."
+  (let ((node (dagma--graph-node-get-ensure graph key)))
+    (setf (dagma-node-parents node) val)))
+
+(defun dagma--graph-children-add-1 (graph key child)
+  "Add CHILD to list as `children' in `dagma-node' node in GRAPH at key KEY."
+  (dagma--node-children-add-1 (dagma--graph-node-get-ensure graph key) child))
+
+(defun dagma--graph-children-remove-1 (graph key child)
+  "Remove CHILD to list as `children' in `dagma-node' node in GRAPH at key KEY."
+  (dagma--node-children-remove-1 (dagma--graph-node-get-ensure graph key) child))
+
+(defun dagma--graph-parents-add-1 (graph key parent)
+  "Add PARENT to list as `parents' in `dagma-node' node in GRAPH at key KEY."
+  (dagma--node-parents-add-1 (dagma--graph-node-get-ensure graph key) parent))
+
+(defun dagma--graph-parents-remove-1 (graph key parent)
+  "Remove PARENT to list as `parents' in `dagma-node' node in GRAPH at key KEY."
+  (dagma--node-parents-remove-1 (dagma--graph-node-get-ensure graph key) parent))
+
+
+;;; Primitive functions to control plist in graph
+
+(defun dagma--graph-plist-get (graph key prop)
+  "Look up KEY in GRAPH to get `dagma-node' and get plist value on PROP."
+  (dagma--node-plist-get (dagma--graph-node-get-ensure graph key) prop))
+
+(defun dagma--graph-plist-put (graph key prop val)
+  "Look up KEY in GRAPH to get `dagma-node' and put plist value to VAL on PROP."
+  (dagma--node-plist-put (dagma--graph-node-get-ensure graph key) prop val))
+
 (provide 'dagma)
 ;;; dagma.el ends here
